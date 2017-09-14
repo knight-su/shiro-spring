671186233bc990dc4b6ff3defb9654331ff2b0b9
knight-su <837081248@qq.com>
2017-09-14 16:59:10 +0800

    增加测试类的初始化方法

--- src/test/java/com/suxin/test/ShiroTest.java
+++ src/test/java/com/suxin/test/ShiroTest.java
@@ -1,12 +1,102 @@
 package com.suxin.test;
 
+import com.suxin.dao.UserDao;
+import com.suxin.entity.Permission;
+import com.suxin.entity.Role;
+import com.suxin.entity.User;
+import com.suxin.realm.UserRealm;
+import com.suxin.service.PermissionService;
+import com.suxin.service.RoleService;
+import com.suxin.service.UserService;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-base.xml","classpath:spring-shiro.xml"})
-@Transactional(rollbackFor = Exception.class)
+@TransactionConfiguration(defaultRollback = false)
 public class ShiroTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @Autowired
+    private UserRealm userRealm;
+
+    protected JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private void setDateSource(DataSource dateSource) {
+        jdbcTemplate = new JdbcTemplate(dateSource);
+    }
+
+    protected String password = "123";
+
+    protected Permission p1;
+    protected Permission p2;
+    protected Permission p3;
+    protected Role r1;
+    protected Role r2;
+    protected User u1;
+    protected User u2;
+    protected User u3;
+    protected User u4;
+
+    @Before
+    public void init() {
+        jdbcTemplate.update("delete from sys_users");
+        jdbcTemplate.update("delete from sys_roles");
+        jdbcTemplate.update("delete from sys_permissions");
+        jdbcTemplate.update("delete from sys_users_roles");
+        jdbcTemplate.update("delete from sys_roles_permissions");
+
+
+        //1、新增权限
+        p1 = new Permission("user:create", "用户模块新增", Boolean.TRUE);
+        p2 = new Permission("user:update", "用户模块修改", Boolean.TRUE);
+        p3 = new Permission("menu:create", "菜单模块新增", Boolean.TRUE);
+        permissionService.createPermission(p1);
+        permissionService.createPermission(p2);
+        permissionService.createPermission(p3);
+
+        //2、新增角色
+        r1 = new Role("admin", "管理员", Boolean.TRUE);
+        r2 = new Role("user", "用户管理员", Boolean.TRUE);
+        roleService.createRole(r1);
+        roleService.createRole(r2);
+
+        //3、关联角色-权限
+        roleService.correlationPermissions(r1.getId(), p1.getId());
+        roleService.correlationPermissions(r1.getId(), p2.getId());
+        roleService.correlationPermissions(r1.getId(), p3.getId());
+
+        roleService.correlationPermissions(r2.getId(), p1.getId());
+        roleService.correlationPermissions(r2.getId(), p2.getId());
+
+        //4、新增用户
+        u1 = new User("zhang", password);
+        u2 = new User("li", password);
+        u3 = new User("wu", password);
+        u4 = new User("wang", password);
+        u4.setLocked(Boolean.TRUE);
+        userService.createUser(u1);
+        userService.createUser(u2);
+        userService.createUser(u3);
+        userService.createUser(u4);
+        //5、关联用户-角色
+        userService.correlationRoles(u1.getId(), r1.getId());
+    }
 }
